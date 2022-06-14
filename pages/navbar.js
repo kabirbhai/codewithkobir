9e7fc8a089b0646dd1d43eb20a47c0a28727c74e
kabir <makkobir79@gmail.com>
2022-06-15 02:04:32 +0600

    navbar modified

--- pages/navbar.js
+++ pages/navbar.js
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <header>
-      <nav className="flex justify-between items-center py-10 bg-yellow-100 ">
+    <header className="max-w-screen-2xl mx-auto">
+      <nav className="flex justify-between items-center py-10 px-4 bg-black text-white ">
         <div className="logo text-3xl text-orange-600 font-bold">
           <Link href="/">CodeWithKobir</Link>
         </div>
-        <div className="flex">
+        <div className="flex items-center ">
           <div className="navItem  text-xl mr-14">
-            <ul className="flex space-x-5">
+            <ul className="flex  space-x-5">
               <li>
                 <Link href="/">Home</Link>
               </li>
@@ -26,12 +26,16 @@ const Navbar = () => {
             </ul>
           </div>
           <div className="register">
-            <ul className="flex space-x-3 text-xl">
+            <ul className="flex justify-center items-center space-x-3 text-xl">
               <li>
                 <Link href="/register">Register</Link>
               </li>
               <li>
-                <Link href="/login">Login</Link>
+                <Link href="/login">
+                  <button className="border px-3 py-2 rounded-md bg-blue-600 hover:bg-transparent ease-in">
+                    Login
+                  </button>
+                </Link>
               </li>
             </ul>
           </div>
