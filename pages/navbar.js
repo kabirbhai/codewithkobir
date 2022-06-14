6bcf9fe18b19a9b39bfba26ccd80d62eee67c365
kabir <makkobir79@gmail.com>
2022-06-15 01:43:03 +0600

    navbar finished

--- pages/navbar.js
+++ pages/navbar.js
@@ -1,23 +1,38 @@
 import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <header>
-      <nav className="flex justify-between items-center bg-slate-600 ">
-        <div className="logo">CodeWithKobir</div>
-        <div>
-          <div className="navItem">
-            <ul className="flex">
-              <li>Home</li>
-              <li>Portfolio</li>
-              <li>Blog</li>
-              <li>Contact</li>
+      <nav className="flex justify-between items-center py-10 bg-yellow-100 ">
+        <div className="logo text-3xl text-orange-600 font-bold">
+          <Link href="/">CodeWithKobir</Link>
+        </div>
+        <div className="flex">
+          <div className="navItem  text-xl mr-14">
+            <ul className="flex space-x-5">
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/portfolio">Portfolio</Link>
+              </li>
+              <li>
+                <Link href="/blog">Blog</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact</Link>
+              </li>
             </ul>
           </div>
           <div className="register">
-            <ul>
-              <li>Register</li>
-              <li>Login</li>
+            <ul className="flex space-x-3 text-xl">
+              <li>
+                <Link href="/register">Register</Link>
+              </li>
+              <li>
+                <Link href="/login">Login</Link>
+              </li>
             </ul>
           </div>
         </div>
